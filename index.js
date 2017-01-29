74324ae0dcab2aa26ec71c00f997e0256008e024
Wolf <brucknerw@gmail.com>
2017-01-29 15:44:55 -0500

    grabage code to return json from flat file

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
 
-app.use('/',  helloWorld);
+app.set('port', (process.env.PORT || 9191));
+
+app.get('/',  helloWorld);
+
+app.get('/bye', goodbyeWorld);
+app.get("/recipe/:name", getRecipe);
 
 function helloWorld(req, res, next) {
-	res.send("Hello World.");
+	res.send("Hello world.");
+	console.log("hello...");
 }
+
+function goodbyeWorld(req, res, next) {
+	res.send("GoodBye World");
+	console.log("goodbye...");
+}
+
+function getRecipe(req, res, next) {
+	var name = req.params.name;
+	
+	var recipeJson = getFileContents(name);
+	console.log("recipe requested for %s", name);
+	res.send("How to make " + name);
+	console.log(recipeJson);
+}
+
+function getFileContents(name) {
+	var fileName = name + ".json";
+	console.log(fileName);
+	fs.readFile(fileName, 'utf8', function(err, contents){
+		console.log(contents);
+	})
+}
+;
+
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
