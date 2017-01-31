54937d501bf7f658430581113015d957fc79b075
tom-odon <thomas.odon@gmail.com>
2017-01-30 22:35:42 -0600

    added returnList() method to serve recipe list

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 
 router.use('/list', require('./list'));
 
+
 router.get('/', function(req, res) {
   res.send('Home Page');
 });
--- routes/list.js
+++ routes/list.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
 
 router.get('/', function(req, res) {
-	res.send('Hello, you have requested a list or recipes');
+     //return the list of recipes
+     res.send(returnList());
+     res.end();
 });
+
+function returnList(){
+     var listPath = path.join(__dirname, '..', 'recipes', 'recipelist.json');
+     var listJson = fs.readFileSync(listPath, 'utf8');
+     var data = JSON.parse(listJson);
+     return data;
+}
+
+
 
 module.exports = router;
