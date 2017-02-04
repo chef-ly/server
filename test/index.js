be00fe0aadc380ef739b786ab58194bcebfbd34c
David Chang <davidshchang@gmail.com>
2017-02-04 16:43:30 -0600

    added mocha and chai unit test script

---
+++ test/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var expect = require('chai').expect;
+var request = require('request');
+
+describe('check routes', function() {
+  var port = process.env.PORT || 5000;
+  var url = 'http://localhost:' + port;
+
+  describe('/', function() {
+    it('returns status of 200', function(done) {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  describe('/list', function() {
+    it('return status of 200', function(done) {
+      request(url+'/list', function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+  
+  describe('/recipe/:id', function() {
+    it('return status of 200', function(done) {
+      request(url+'/recipe/1', function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  describe('/bad', function() {
+    it('return status of 404 not found error', function(done) {
+      request(url+'/bad', function(error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+});
