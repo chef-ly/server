8f3694e9c7405901e83c86e0c9c7d40f8f18652d
David Chang <davidshchang@gmail.com>
2017-05-02 20:05:11 -0500

    basic website framework

--- routes/index.js
+++ routes/index.js
@@ -30,8 +30,5 @@ router.use('/list', require('./list'));
 router.use('/recipe', require('./recipe'));
 router.use('/search', require('./search'));
 router.use('/user', verify, require('./user'));
-router.get('/', verify, function(req, res) {
-  res.send('Home Page');
-});
 
 module.exports = router;
