fee064b0110b5fb3a320b51ae3b8467ecbc4345a
David Chang <davidshchang@gmail.com>
2017-05-05 20:07:40 -0500

    added jquery

---
+++ public/js/chefly.js
@@ -0,0 +1,29 @@
+$(function() {
+  $('#send').on('click', function() {
+    var email = $('#eml').val();
+
+    if (validateEmail(email)) {
+      $.ajax({
+        url: '/alpha/signup/' + email,
+      })
+      .success(function(data) {
+        $('#error').hide();
+        $('#success').text("Awesome! You'll hear from us soon!").show();
+      })
+      .fail(function(err){
+        $('#error').text('Oops, there was a problem saving your email address!').show();
+      });
+    } else {
+      $('#error').text('Invalid email address').show();
+    }
+  });
+});
+
+function validateEmail(eml) {
+  var s = eml.split('@');
+  if (s.length == 2 && s[0].length > 0 && s[1].length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
